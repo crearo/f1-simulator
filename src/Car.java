06e6b43cc0361734b8bbb568312ddb50478d2d25
CreaRo <bhardwaj.rish@gmail.com>
2015-10-11 20:59:23 +0530

    added getters and setters

--- src/Car.java
+++ src/Car.java
@@ -1,22 +1,85 @@
 public class Car {
 
-	public int teamNumber;
-	public double topSpeed;
-	public int acceleration;
-	public boolean nitroUsed;
-	public double hF;
+	private int teamNumber;
+	private double topSpeed;
+	private int acceleration;
+	private boolean nitroUsed;
+	private double handlingFactor;
+	private double currentDistanceCovered;
+	private double currentSpeed;
+	private boolean reduceSpeed;
 
-	public double currentDistanceCovered;
-	public double currentSpeed;
-	public boolean reduceSpeed;
+	public int getTeamNumber() {
+		return teamNumber;
+	}
+
+	public void setTeamNumber(int teamNumber) {
+		this.teamNumber = teamNumber;
+	}
+
+	public double getTopSpeed() {
+		return topSpeed;
+	}
+
+	public void setTopSpeed(double topSpeed) {
+		this.topSpeed = topSpeed;
+	}
+
+	public int getAcceleration() {
+		return acceleration;
+	}
+
+	public void setAcceleration(int acceleration) {
+		this.acceleration = acceleration;
+	}
+
+	public boolean isNitroUsed() {
+		return nitroUsed;
+	}
+
+	public void setNitroUsed(boolean nitroUsed) {
+		this.nitroUsed = nitroUsed;
+	}
+
+	public double getHandlingFactor() {
+		return handlingFactor;
+	}
+
+	public void setHandlingFactor(double hF) {
+		this.handlingFactor = hF;
+	}
+
+	public double getCurrentDistanceCovered() {
+		return currentDistanceCovered;
+	}
+
+	public void setCurrentDistanceCovered(double currentDistanceCovered) {
+		this.currentDistanceCovered = currentDistanceCovered;
+	}
+
+	public double getCurrentSpeed() {
+		return currentSpeed;
+	}
+
+	public void setCurrentSpeed(double currentSpeed) {
+		this.currentSpeed = currentSpeed;
+	}
+
+	public boolean isReduceSpeed() {
+		return reduceSpeed;
+	}
+
+	public void setReduceSpeed(boolean reduceSpeed) {
+		this.reduceSpeed = reduceSpeed;
+	}
 
 	public Car(int TOTAL_CARS, int i) {
 		this.teamNumber = i;
-		this.topSpeed = 5 * (150 + (20 * i)) / 18;
-		this.acceleration = (2 * TOTAL_CARS - i);
+		this.topSpeed = 5 * (Constants.BASE_SPEED + (Constants.THRESHOLD_SPEED * i)) / 18;
+		this.acceleration = (Constants.BASE_ACCELERATION - Constants.THRESHOLD_ACCN * i);
 		this.nitroUsed = false;
-		this.hF = 0.8;
-		this.currentDistanceCovered = (TOTAL_CARS - i) * 200;
+		this.handlingFactor = Constants.HANDLING_FACTOR;
+		this.currentDistanceCovered = ((TOTAL_CARS - i) * Constants.INIT_START_DISTANCE);
 		this.currentSpeed = 0;
 		this.reduceSpeed = false;
 	}
